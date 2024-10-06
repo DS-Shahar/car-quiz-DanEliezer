3733604c7dd62392df3d76d409e99501b8b5bc1f
DanEliezer <eliezerdan09@gmail.com>
2024-10-06 12:41:53 +0300

    Add files via upload
    מתוקן

--- CameraInfo.java
+++ CameraInfo.java
@@ -1,5 +1,30 @@
 package bohan;
 
 public class CameraInfo {
+    private int city;
+    private int maxSpeed;
+    private CarInfo[] cars;  
+    
+    public CameraInfo(int city, int maxSpeed, CarInfo[] cars) {
+        this.city = city;
+        this.maxSpeed = maxSpeed;
+        this.cars = cars;
+    }
+    
+    public int getCity() {
+        return city;
+    }
+    
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
 
+    public boolean allGood() {
+        for (CarInfo car : cars) {
+            if (car.illegal(maxSpeed)) {
+                return false; 
+            }
+        }
+        return true; 
+    }
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,47 @@
 package bohan;
 
 public class Main {
+    public static int legalCities(CameraInfo[] cameras) {
+        int count = 0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        for (int i = 0; i < cameras.length; i++) {
+            CameraInfo camera = cameras[i];
+            if (camera == null) continue;
+            boolean allCamerasGoodForCity = true;
+            for (int j = i; j < cameras.length; j++) {
+                if (cameras[j] != null && cameras[j].getCity() == camera.getCity()) {
+                    if (!cameras[j].allGood()) {
+                        allCamerasGoodForCity = false;
+                        break;
+                    }
+                }
+            }
+            if (allCamerasGoodForCity) {
+                count++;
+                for (int j = i + 1; j < cameras.length; j++) {
+                    if (cameras[j] != null && cameras[j].getCity() == camera.getCity()) {
+                        cameras[j] = null; 
+                    }
+                }
+            }
+        }
 
-	}
+        return count;
+    }
 
+    public static void main(String[] args) {
+        CarInfo[] cars1 = new CarInfo[2];
+        cars1[0] = new CarInfo("c10", true, 90);
+        cars1[1] = new CarInfo("c11", false, 120);
+
+        CarInfo[] cars2 = new CarInfo[2];
+        cars2[0] = new CarInfo("c20", false, 45);
+        cars2[1] = new CarInfo("c21", false, 50);
+
+        CameraInfo[] cameras = new CameraInfo[2];
+        cameras[0] = new CameraInfo(12, 110, cars1);
+        cameras[1] = new CameraInfo(12, 50, cars2);
+
+        System.out.println("Number of cities with all legal cars: " + legalCities(cameras));
+    }
 }
